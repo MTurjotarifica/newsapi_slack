8a91ff24a57d6a47798cb9b4416e16e6d19fd1f4
MTurjotarifica <116021103+MTurjotarifica@users.noreply.github.com>
2023-05-11 21:38:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 import requests
 import json
 
-
 import pandas as pd
 from openpyxl import load_workbook
 from newsdataapi import NewsDataApiClient
@@ -38,79 +37,80 @@ def interactive_trigger():
     data2 = request.form.to_dict()
     user_id = data.get('user_id')
     channel_id = json.loads(data2['payload'])['container']['channel_id']
-    text = data.get('text')
+    text = json.loads(data2['payload'])['actions'][0]['value']
     print (text)
+
     response_url = json.loads(data2['payload'])['response_url']
     actions = data.get("actions")
     actions_value = data.get("actions.value")
     action_id = json.loads(data2['payload'])['actions'][0]['action_id']
 
-    client.chat_postMessage(channel=channel_id, text="A backgroundworker is running your task. Please wait.")
+    client.chat_postMessage(channel=channel_id, text=text)
         
-    if action_id == "newsapi":
-      today = datetime.datetime.now().date()
+    if action_id == "newsapi":       
+        today = datetime.datetime.now().date()
 
-      api = NewsDataApiClient(apikey="pub_205194b814f4b3a8ef344988313fe445954eb")
-      response = api.news_api(q=text, country='de')
-      articles = response['results']
+        api = NewsDataApiClient(apikey="pub_205194b814f4b3a8ef344988313fe445954eb")
+        response = api.news_api(q=text, country='de')
+        articles = response['results']
 
-      article_list = []
-      for article in articles:
-          pub_date = datetime.datetime.strptime(article['pubDate'], '%Y-%m-%d %H:%M:%S').date()
-          if (today - pub_date).days < 3:
-              category = [c.lower() for c in article.get('category', [])]  # Get the category key and convert each category to lowercase
-              if 'sports' not in category:
-                  # Check if 'description' key exists and if it is a string before calling translate_text()
-                  description = article.get('description', None)
-                  description_translated = translate_text(description) if description else ''
-                      
-                  # Check if 'content' key exists and if it is a string before calling translate_text()
-                  content = article.get('content', None)
-                  content_translated = translate_text(content) if content else ''
-                  
-                  # Check if the keyword 'Telekom Baskets Bonn' is in the content or description
-                  if 'Telekom Baskets Bonn' not in content_translated and 'Telekom Baskets Bonn' not in description_translated:
-                      article_dict = {'Title': translate_text(article['title']), 
-                                      'Link': article['link'], 
-                                      'Keywords': article['keywords'], 
-                                      'Creator': article['creator'], 
-                                      'Content': content_translated, 
-                                      'Description': description_translated, 
-                                      'PubDate': article['pubDate'], 
-                                      'Image URL': article['image_url'], 
-                                      'Category': category}
-                      article_list.append(article_dict)
+        article_list = []
+        for article in articles:
+            pub_date = datetime.datetime.strptime(article['pubDate'], '%Y-%m-%d %H:%M:%S').date()
+            if (today - pub_date).days < 3:
+                category = [c.lower() for c in article.get('category', [])]  # Get the category key and convert each category to lowercase
+                if 'sports' not in category:
+                    # Check if 'description' key exists and if it is a string before calling translate_text()
+                    description = article.get('description', None)
+                    description_translated = translate_text(description) if description else ''
+                        
+                    # Check if 'content' key exists and if it is a string before calling translate_text()
+                    content = article.get('content', None)
+                    content_translated = translate_text(content) if content else ''
+                    
+                    # Check if the keyword 'Telekom Baskets Bonn' is in the content or description
+                    if 'Telekom Baskets Bonn' not in content_translated and 'Telekom Baskets Bonn' not in description_translated:
+                        article_dict = {'Title': translate_text(article['title']), 
+                                        'Link': article['link'], 
+                                        'Keywords': article['keywords'], 
+                                        'Creator': article['creator'], 
+                                        'Content': content_translated, 
+                                        'Description': description_translated, 
+                                        'PubDate': article['pubDate'], 
+                                        'Image URL': article['image_url'], 
+                                        'Category': category}
+                        article_list.append(article_dict)
 
 
-      df_new = pd.DataFrame(article_list)
+        df_new = pd.DataFrame(article_list)
 
-      # Send the new links and translated text to Slack
-      for index, row in df_new.iterrows():
-          link = row['Link']
-          content = row['Content']
-          description = row['Description']
-          title = translate_text(row['Title'])
+        # Send the new links and translated text to Slack
+        for index, row in df_new.iterrows():
+            link = row['Link']
+            content = row['Content']
+            description = row['Description']
+            title = translate_text(row['Title'])
 
-          if content:
-              # Send the content if it exists
-              message = content
-          elif description:
-              # Send the description if content does not exist but description exists
-              message = description
-          else:
-              # Send the title if neither content nor description exists
-              message = title
+            if content:
+                # Send the content if it exists
+                message = content
+            elif description:
+                # Send the description if content does not exist but description exists
+                message = description
+            else:
+                # Send the title if neither content nor description exists
+                message = title
 
-          try:
-              response = client.chat_postMessage(
-                  channel=channel_id,
-                  text=f"• <{link}|{title}>\n{message}\n",
-                  unfurl_links=False,
-              )
-          except SlackApiError as e:
-              print("Error sending message to Slack: {}".format(e))
-              
-      return 'ok', 200
+            try:
+                response = client.chat_postMessage(
+                    channel=channel_id,
+                    text=f"• <{link}|{title}>\n{message}\n",
+                    unfurl_links=False,
+                )
+            except SlackApiError as e:
+                print("Error sending message to Slack: {}".format(e))
+                
+        return 'ok', 200
       
         
 # Add a route for the /hello command
